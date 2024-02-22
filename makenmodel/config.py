759d90a5c123d91b2911797cb5ae01777b3fedb1
Carter Gracin <carter.rgracin@gmail.com>
2024-02-21 23:09:38 -0500

    added venv stuff

---
+++ makenmodel/config.py
@@ -0,0 +1,13 @@
+'''Configuration file for project'''
+
+import pathlib
+
+APPLICATION_ROOT = '/'
+
+MAKENMODEL_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = pathlib.Path('/var/www/uploads')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jped', 'gif', 'heic'])
+
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+DATABASE_FILENAME = MAKENMODEL_ROOT/'var'/'makenmodel.sqlite3'
