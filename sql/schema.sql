b6cf4b9d6b46ee0d28b0e782bc389893a07e7b7e
Carter Gracin <carter.rgracin@gmail.com>
2024-02-29 07:34:36 -0500

    made table

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,25 +12,25 @@ CREATE TABLE users(
 -- Paints table
 CREATE TABLE paints(
     brand VARCHAR(50),
-    unique_paint_identifier INTEGER AUTOINCREMENT,
+    unique_paint_identifier INTEGER PRIMARY KEY AUTOINCREMENT,
     paint_code VARCHAR(256),
     background_color VARCHAR(12),
     shine_type VARCHAR(20),
     paint_type VARCHAR(20),
-    PRIMARY KEY (brand, unique_paint_identifier)
+    FOREIGN KEY (brand) REFERENCES brands(brand)
 );
 
 -- Brands table
 CREATE TABLE brands(
     brand VARCHAR(50),
     num_paints INTEGER,
-    unique_brand_identifier INTEGER AUTOINCREMENT PRIMARY KEY
+    unique_brand_identifier INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
 -- User_Paints table
 CREATE TABLE user_paints(
     username VARCHAR(256),
-    unique_paint_identifier VARCHAR(256),
+    unique_paint_identifier INTEGER,
     FOREIGN KEY (username) REFERENCES User(username) ON DELETE CASCADE,
     FOREIGN KEY (unique_paint_identifier) REFERENCES Paints(unique_paint_identifier) ON DELETE CASCADE,
     PRIMARY KEY (username, unique_paint_identifier)
