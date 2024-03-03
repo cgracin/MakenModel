27dc7472ca9988c88d458503cd0de41cf5b8e38a
Carter Gracin <carter.rgracin@gmail.com>
2024-03-02 19:41:55 -0500

    added logic to add paints to user database

--- makenmodel/views/toolbox.py
+++ makenmodel/views/toolbox.py
@@ -45,3 +45,56 @@ def show_your_paints():
     context['logname'] = logname
 
     return flask.render_template('your_paints.html', **context)
+
+
+@makenmodel.app.route('/toolbox/add-paints/', methods=['POST'])
+def add_paints():
+    '''Route to add paint to database'''
+    context = {}
+
+    connection = makenmodel.model.get_db()
+
+    logname = flask.session['username']
+    context['logname'] = logname
+
+    brand = flask.request.form['brand']
+    paint_info = flask.request.form['paint']
+
+    # print(paint_info)
+    paint_info = paint_info.split(' ')
+
+    paint_code = paint_info[0]
+
+    paint_type = paint_info[-1]
+
+    paint_info.pop(0)
+    paint_info.pop(-1)
+
+    paint_name = (' ').join(paint_info)
+    # slice off the ()
+    paint_code = paint_code[1:-1]
+
+    # slice off the ()
+    paint_type = paint_type[1:-1]
+
+    print(paint_code, paint_name, paint_type)
+
+    context['brand'] = brand
+
+    cur = connection.execute(
+        "SELECT unique_paint_identifier FROM paints "
+        "WHERE brand = ? AND paint_code = ? AND paint_name = ? AND paint_type = ?",
+        (brand, paint_code, paint_name, paint_type)
+    )
+
+    identifier = cur.fetchone()['unique_paint_identifier']
+
+    # Inserting username and identifier into db
+    connection.execute(
+        "INSERT OR IGNORE INTO user_paints (username, unique_paint_identifier) "
+        "VALUES (?, ?)",
+        (logname, identifier)
+    )
+    connection.commit()
+
+    return flask.render_template('add_paints.html', **context)
