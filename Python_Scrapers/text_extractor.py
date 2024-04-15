735bb13707937c51900e65489cc5106d6bb84abc
supermathtm <aprilyc@umich.edu>
2024-04-15 12:38:15 -0400

    some changes

--- Python_Scrapers/text_extractor.py
+++ Python_Scrapers/text_extractor.py
@@ -7,128 +7,34 @@ from pdf2image import convert_from_path
 import PyPDF2
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
+from google.cloud import storage
 import requests  # type: ignore
-
-# TODO(developer): Uncomment these variables before running the sample.
 
 PAGE_LINK_OUTPUT = "vehicle_model_links.output"
 MODEL_PDF_LINKS = "model_pdfs.output"
-PDF_TEXT_FOLDER = "pdf_extracted"
+PDF_PROCESSED_FOLDER = "pdf_preprocessed"
 
-def quickstart(project_id: str, location: str, file_path: str, processor_display_name: str = "My Processor",):
-    # You must set the `api_endpoint`if you use a location other than "us".
-    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
-    client = documentai.DocumentProcessorServiceClient(client_options=opts)
+def list_blobs():
+    """Lists all the blobs in the bucket."""
+    bucket_name = "makenmodel-pdf"
 
-    # The full resource name of the location, e.g.:
-    # `projects/{project_id}/locations/{location}`
-    parent = client.common_location_path(project_id, location)
+    storage_client = storage.Client()
 
-    # Create a Processor
-    processor = client.create_processor(
-        parent=parent,
-        processor=documentai.Processor(
-            type_="OCR_PROCESSOR",  # Refer to https://cloud.google.com/document-ai/docs/create-processor for how to get available processor types
-            display_name=processor_display_name,
-        ),
-    )
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
 
-    # Print the processor information
-    print(f"Processor Name: {processor.name}")
-
-    # Read the file into memory
-    with open(file_path, "rb") as image:
-        image_content = image.read()
-
-    # Load binary data
-    raw_document = documentai.RawDocument(
-        content=image_content,
-        mime_type="application/pdf",  # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
-    )
-
-    # Configure the process request
-    # `processor.name` is the full resource name of the processor, e.g.:
-    # `projects/{project_id}/locations/{location}/processors/{processor_id}`
-    request = documentai.ProcessRequest(name=processor.name, raw_document=raw_document)
-
-    result = client.process_document(request=request)
-
-    # For a full list of `Document` object attributes, reference this page:
-    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
-    document = result.document
-
-    # Read the text recognition output from the processor
-    print("The document contains the following text:")
-    print(document.text)
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        print(blob.name)
 
 
-def extract_text():
-    if not os.path.exists(MODEL_PDF_LINKS):
-        print("PDF links file not found. Please run the script with -p option first.")
-        return
 
-    if not os.path.exists(PDF_TEXT_FOLDER):
-        os.makedirs(PDF_TEXT_FOLDER)
-
-    with open(MODEL_PDF_LINKS, "r", encoding="utf-8") as links:
-        for pdf_link in links.readlines():
-            pdf_link = pdf_link.strip()
-            # Extract text from image of PDF
-            image_text = extract_text_from_pdf_image(pdf_link)
-
-            if image_text:
-                filename = pdf_link.split("/")[-1]
-                output_file_path = os.path.join(PDF_TEXT_FOLDER, filename + ".txt")
-                with open(output_file_path, "w", encoding="utf-8") as text_file:
-                    text_file.write(image_text + "\n\n\n\n")
-
-
-def extract_text_from_pdf_image(pdf_url):
-    base_url = "https://www.scalemates.com"
-    full_link = urljoin(base_url, pdf_url)
-    response = requests.get(full_link)
-    if response.status_code == 200:
-        with tempfile.NamedTemporaryFile(delete=True) as temp_file:
-            temp_file.write(response.content)
-            temp_file.flush()
-            # Convert the PDF to images
-            images = convert_from_path(temp_file.name)
-            extracted_text = ""
-            for image in images:
-                # Use pytesseract to perform OCR on the image
-                text = pytesseract.image_to_string(image)
-                extracted_text += text + "\n"
-            return extracted_text
-    else:
-        print("Failed to download PDF file:", pdf_url)
-        return ""
 
 def main():
-    project_id = "eecs486-makenmodel"
-    location = "us"  # Format is "us" or "eu"
-    file_path = "../pdf_extracted/100002-55-instructions.pdf"
-    processor_display_name = "makenmodel2" # Must be unique per project, e.g.: "My Processor"
-    quickstart(project_id, location, file_path, processor_display_name)
-    # if os.path.exists(PDF_TEXT_FOLDER):
-    #     print("Removing existing PDF text folder and files...")
-    #     shutil.rmtree(PDF_TEXT_FOLDER)
-    # # extract_text()
-    # if not os.path.exists(PDF_TEXT_FOLDER):
-    #     os.makedirs(PDF_TEXT_FOLDER)
-    # base_url = "https://www.scalemates.com"
+    if not os.path.exists(PDF_PROCESSED_FOLDER):
+        os.makedirs(PDF_PROCESSED_FOLDER)
 
-    # with open(MODEL_PDF_LINKS, "r", encoding="utf-8") as file:
-    #     for link in file.readlines():
-    #         pdf_url = link.strip()
-    #         full_link = urljoin(base_url, pdf_url)
-    #         response = requests.get(full_link)
-    #         if response.status_code == 200:
-    #             filename = pdf_url.split("/")[-1]
-    #             output_file_path = os.path.join(PDF_TEXT_FOLDER, filename)
-    #             with open(output_file_path, "wb") as pdf:
-    #                 pdf.write(response.content)
-    #         else:
-    #             print(f"Failed to download PDF: {pdf_url}")
+    list_blobs()
 
 
 if __name__ == "__main__":
