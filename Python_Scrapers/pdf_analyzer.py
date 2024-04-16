83c3c4f268363e1caac8c2ffba449e21024dca3b
supermathtm <aprilyc@umich.edu>
2024-04-16 11:53:42 -0400

    pdf_analyzer has some code

--- Python_Scrapers/pdf_analyzer.py
+++ Python_Scrapers/pdf_analyzer.py
@@ -1,100 +1,46 @@
 import sys
 import os
 import json
-import math
-import re
-import flask
-import pathlib
-import sqlite3
+from classifier_paint_part import *
+from classifier_language import *
 
-# pseudocode
-# 1. Get user's colors
-# 2. Get total colors from pdf
-# 3. Calculate color score
+EXTRACTED_JSON_FOLDER = "json_extracted"
 
-COLOR_CODES = []
-
-def get_color_codes_json(data, paint_types):
-    """Getting color codes from JSON."""
-    do_not_add = ["Light Earth", "Mud", "Sand"]
-    color_codes = []
-    for paint_type in paint_types:
-        for i in data[paint_type]:
-            if i[0] not in do_not_add and not i[0].isdigit():
-                color_codes.append(i[0])
-    return color_codes
-
-def get_parts_and_paints_from_instructions(directory):
-    files = os.listdir(directory)
-
-    for file in files:
-        path = os.path.join(directory, file)
-        json_data = extract_json_from_file(path)
-        if json_data:
-            text_content = json_data["text"]
-            print(text_content)
-
-def extract_json_from_file(file_path):
+def retrieve_json(file_path):
     """Retrieve JSON from files."""
     with open(file_path, "r", encoding="iso-8859-1") as file:
         try:
             json_file = json.load(file)
         except json.JSONDecodeError as e:
+            print(f"Error retrieving JSON: {file_path}")
             return None
         return json_file
 
-def dict_factory(cursor, row):
-    """Convert database row objects to a dictionary keyed on column name.
-
-    This is useful for building dictionaries which are then used to render a
-    template.  Note that this would be inefficient for large queries.
-    """
-    return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
-
-def get_db():
-    """Open a new database connection."""
-    root_folder = pathlib.Path(__file__).resolve().parent.parent
-    DATABASE_FILENAME = root_folder / 'var' / 'makenmodel.sqlite3'
-    connection = sqlite3.connect(str(DATABASE_FILENAME))
-    connection.row_factory = dict_factory
-    connection.execute("PRAGMA foreign_keys = ON")
-    return connection
-
-def get_user_paints():
-    logname = 'test_user'
-    connection = get_db()
-    cur = connection.execute(
-        "SELECT p.paint_code "
-        "FROM user_paints up "
-        "JOIN paints p ON up.unique_paint_identifier = p.unique_paint_identifier "
-        "WHERE up.username = ?",
-        (logname,)
-    )
-    results = cur.fetchall()
-    codes = [result['paint_code'] for result in results]
-    return codes
-
+def get_detected_langs(json_pages):
+    """Retrieve translated languages from JSON"""
+    languages = set()
+    for pages in json_pages:
+        detected_langs = pages["detectedLanguages"]
+        for lang in detected_langs:
+            lang_code = lang["languageCode"]
+            if lang_code != "en":
+                languages.add(lang_code)
+    return languages
 
 def main():
     """Analyze extracted PDF text"""
-    # Paints to grab: Tamiya, Tamiya Color Laqcuer Paint, Tamiya Paint Markers, Tamiya Polycarb Marker
-    paints_grab_list = [
-        "Tamiya",
-        "Tamiya Color Lacquer Paint",
-        "Tamiya Paint Markers",
-        "Tamiya Polycarb Marker",
-    ]
-    # Opening JSON file
-    with open(os.path.join("scraping_data/paint_scrape_data.json"), "r", encoding="utf-8") as paint_data:
-        data = json.load(paint_data)
-        color_codes = get_color_codes_json(data, paints_grab_list)
+    json_directory = os.listdir(EXTRACTED_JSON_FOLDER)
+    instruction_texts = {}
 
-    instruction_folder = "json_extracted"
-    get_parts_and_paints_from_instructions(instruction_folder)
+    for file in json_directory:
+        path = os.path.join(EXTRACTED_JSON_FOLDER, file)
+        json_data = retrieve_json(path)
+        if json_data:
+            json_text = json_data["text"]
+            text_langs = get_detected_langs(json_data["pages"])
 
-    # get_user_paints()
-
-    return None
+            json_text = get_parts_and_paints_from_instructions(json_text)
+            processed_text = get_en_text(json_text, text_langs)
 
 
 if __name__ == "__main__":
