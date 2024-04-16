d69340e246d5b831b1232259ad56471e9600c495
sawickid <sawickid@umich.edu>
2024-04-15 22:56:05 -0400

    added scale scraping

--- Python_Scrapers/pdf_scraper.py
+++ Python_Scrapers/pdf_scraper.py
@@ -11,10 +11,13 @@ from urllib.parse import urljoin
 import pytesseract
 from pdf2image import convert_from_path
 import shutil
+import re
 
 PAGE_LINK_OUTPUT = 'model_page_links.output'
 MODEL_PDF_LINKS = 'pdf_links.output'
+MODEL_SPEC_INFO = 'model_specs.output'
 PDF_TEXT_FOLDER = 'pdf_texts'
+SCALE_DIFF_SCORES = ''
 
 def get_model_urls(page_url):
     if os.path.exists(PAGE_LINK_OUTPUT):
@@ -40,6 +43,29 @@ def get_model_urls(page_url):
         for div in divs:
             link = div.find('a', class_='pf')['href']
             output.write(link + '\n')
+
+def get_specs():
+
+    with open(MODEL_SPEC_INFO, 'w', encoding='utf-8') as out:
+        out.write('')
+
+    with open(PAGE_LINK_OUTPUT, 'r', encoding='utf-8') as links:
+        page_links = [link.strip() for link in links.readlines()]
+
+    for link in page_links:
+        base_url = "https://www.scalemates.com"
+        full_link = urljoin(base_url, link)
+        page = requests.get(full_link)
+
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            specs = soup.find('dd', class_='p4').text.strip()
+            title = re.search("Title:.*Number", specs).group()[6:-6]
+            scale = re.search("Scale:.*Type", specs).group()[6:-4]
+            with open ()
+            with open(MODEL_SPEC_INFO, 'a', encoding='utf-8') as output:
+                output.write(title + ', ' + scale + '\n')
 
 def get_pdfs():
 
@@ -131,6 +157,8 @@ def main():
         get_model_urls(url)
     elif mode == '-p':
         get_pdfs()
+    elif mode == '-s':
+        get_specs()
     elif mode == '-t':
         if os.path.exists(PDF_TEXT_FOLDER):
             print("Removing existing PDF text folder and files...")
