fcf9bbc47c5d3823bd16e9e4329b64d80ec22ae2
sawickid <sawickid@umich.edu>
2024-04-16 12:21:52 -0400

    removed lang class from diff

--- Python_Scrapers/classifier_difficulty.py
+++ Python_Scrapers/classifier_difficulty.py
@@ -1,29 +1,10 @@
 # NEED:
 # parts / page
-# paints / page
+# paints / parts
 #
 
 # HAVE:
 # scale score
-import json
-from ftlangdetect import detect
-
-# Returns string of all (mostly) english text from an instructions JSON file
-def get_en_text(fname):
-    with open(fname, 'r', encoding='utf-8') as js:
-        text = json.load(js)
-        text = text['text']
-        en_lines = []
-        text = text.split('\n')
-        for line in text:
-            nl = ''
-            for word in line.split():
-                if detect(text=word, low_memory=False)['lang'] not in ['ja', 'de', 'zh'] or word.isdigit():
-                    nl += word + ' '
-            en_lines.append(nl)
-        return ' '.join([line.strip() for line in en_lines if line != ''])
-
-
 def calculate_diff_score(parts, paints, num_pages, keywords, scale_score):
     params = [0.25, 0.25, 0.25, 0.25]
     unique_parts = set(parts)
